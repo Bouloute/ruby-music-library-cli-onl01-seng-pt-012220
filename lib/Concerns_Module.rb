d10a4bb0173e9e2d078621a5553128ebce1837d5
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-24 16:23:10 +0000

    Automatically backed up by Learn

--- lib/Concerns_Module.rb
+++ lib/Concerns_Module.rb
@@ -3,5 +3,11 @@ module Concerns
     def find_by_name(name)
       self.all.find{|obj| obj.name == name}
     end
+
+    def self.find_or_create_by_name(name)
+      obj = self.find_by_name(name)
+      return obj if obj != nil
+      self.create(name)
+    end
   end
 end
